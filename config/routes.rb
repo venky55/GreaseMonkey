13e7f338736a2fe030246981b09b9c6c4040ff9f
Venkatesh Narayanan <venkateshnarayanan@yahoo.com>
2016-11-29 19:33:52 +0530

    user signup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :listings
+  
+  root to: "listings#index"
+  #devise_for :installs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
