71e2f677afd44baebc09f794eaf5acef9bae57f3
venky <venkateshnarayanan@yahoo.com>
2017-02-26 13:05:46 +0530

    image upload done

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'acts-as-taggable-on'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'carrierwave'
+gem 'fog'
 gem 'mini_magick'
 # Use Unicorn as the app server
 # gem 'unicorn'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,15 @@
+
+CarrierWave.configure do |config|
+	 config.fog_credentials = {
+      :provider               => "AWS",
+      :aws_access_key_id      => "AKIAI5VD7SAEVVBYGS3A",
+      :aws_secret_access_key  => "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",
+      :region                 => 'ap-southeast-1' # Change this for different AWS region. Default is 'us-east-1'
+  }
+  config.fog_directory  = "kranq"
+end
+Excon.ssl_verify_peer = false 
+
 module CarrierWave
   module MiniMagick
     def quality(percentage)
